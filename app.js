84a7ce538c5047b3970adc23015d0501ae3dee72
codewithishwar-dev <codewithishwar@gmail.com>
2026-04-23 17:31:19 +0530

    Node pratise

---
+++ app.js
@@ -0,0 +1,8 @@
+//import {x,doSumOfTwoNumber} from "./sum.js";
+const {x, doSumOfTwoNumber} = require("./sum.js");
+const a = 10;
+const b = 20;
+const c = 200;
+console.log(x);
+doSumOfTwoNumber(a ,b);
+
---
+++ sum.js
@@ -0,0 +1,7 @@
+const x = 100;
+function doSumOfTwoNumber(x, y){
+  const c = x + y;
+  console.log(c);
+}
+
+module.exports = { x , doSumOfTwoNumber};
